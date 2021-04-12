508710c4a9a4d2ffb25451f49e5ed598549cfcea
luxis <luxisgrublys@gmail.com>
2021-04-12 10:14:32 +0300

    Metodu pavadinimu update

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\MainController::class, 'index'])->name('home');
 Route::get('/profile', [App\Http\Controllers\MainController::class, 'showProfile'])->name('profile');
-Route::get('/contact_info', [App\Http\Controllers\UsersController::class, 'showContactInfo'])->name('contact_info');
-Route::get('/edit_contact_info/{id}', [App\Http\Controllers\UsersController::class, 'editContactInfo']);
-Route::post('edit',[App\Http\Controllers\UsersController::class,'updateContactInfo']);
+Route::get('/contact_info', [App\Http\Controllers\UsersController::class, 'showPersonalData'])->name('contact_info');
+Route::get('/edit_contact_info/{id}', [App\Http\Controllers\UsersController::class, 'showClientPersonalData']);
+Route::post('edit',[App\Http\Controllers\UsersController::class,'updateData']);
