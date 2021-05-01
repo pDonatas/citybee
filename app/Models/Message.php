9610f9472aa1de09d6e079d39a1699ab502a54f1
Donatas <doantasp060@gmail.com>
2021-05-01 23:47:17 +0300

    Specialistu chato posisteme

---
+++ app/Models/Message.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'sender',
+        'receiver',
+        'message'
+    ];
+
+    public function chat() {
+        return $this->belongsTo(Chat::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerSuccessSpecialists\CustomerSuccessSpecialistController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,3 +27,14 @@ Route::get('/wallet', [App\Http\Controllers\WalletController::class, 'openWallet
 Route::get('/add_money_to_wallet', [App\Http\Controllers\WalletController::class, 'addMoney']);
 Route::post('addMoney', [App\Http\Controllers\WalletController::class, 'updateBalance']);
 
+
+//Customer success specialist
+Route::resource('/help', CustomerSuccessSpecialistController::class);
+Route::post('/help/write/{id}', [CustomerSuccessSpecialistController::class, 'write'])->name('help.write');
+Route::get('/help/chats', [CustomerSuccessSpecialistController::class, 'showChats'])->name('help.chats');
+Route::get('/help/accept/{id}', [CustomerSuccessSpecialistController::class, 'accept'])->name('help.accept');
+Route::get('/help/chat/{id}', [CustomerSuccessSpecialistController::class, 'chat'])->name('help.chat');
+Route::get('/help/car/connect/{id}', [CustomerSuccessSpecialistController::class, 'connect'])->name('help.car.connect');
+Route::get('/help/car/technical/{id}', [CustomerSuccessSpecialistController::class, 'technical'])->name('help.car.technical');
+Route::get('/help/discount/{id}', [CustomerSuccessSpecialistController::class, 'discount'])->name('help.discount');
+Route::get('/help/ban/{id}', [CustomerSuccessSpecialistController::class, 'ban'])->name('help.ban');
